2c7b09efc02cc08c542de048c7ae74ff424beee5
DanielYakubov <Danielyak98@gmail.com>
2023-03-29 22:38:32 -0400

    addressed comments, switched to torch, added argparse

--- udtube.py
+++ udtube.py
@@ -4,28 +4,29 @@ https://github.com/Kyubyong/nlp_made_easy/blob/master/Pos-tagging%20with%20Bert%
 import torch
 import transformers
 import torch.nn as nn
-from tqdm import tqdm
-from torch.utils import data
+import lightning.pytorch as pl
+import argparse
 
 
-class UDTube(nn.Module):
+def set_up_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", help="The model that will be used for inference")
+    parser.add_argument("--training_input", help="Training data, conllu format")
+    parser.add_argument("--predict_input", help="The data we will be predicting on, conllu format")
+    parser.add_argument("--predict_output", help="The file we will be writing to, conllu format")
+    parser.add_argument("--batch_size", default=32, type=int, help="Batch size")
+    return parser
+
+
+class UDTube(pl.LightningModule):
     def __init__(self, model_name, pos_out_size=None, um_out_size=None, lem_vocab_size=None):
         super().__init__()
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name, return_tensors="pt")
-        self.bert = transformers.AutoModel.from_pretrained('bert-base-cased', output_hidden_states=True)
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.lin_pos = nn.Linear(768, pos_out_size)
-        self.lin_um = nn.Linear(768, um_out_size)
-        self.lin_lem = nn.Linear(768, lem_vocab_size)
-
-    @staticmethod
-    def _inplace_mean(tensors):
-        """helper to get inplace mean of tensors"""
-        tmp = tensors[0]
-        for i in range(1, len(tensors)):
-            tmp *= tensors[i]
-        avg_tensor = tmp / len(tensors)
-        return avg_tensor
+        self.bert = transformers.AutoModel.from_pretrained(model_name, output_hidden_states=True)
+        self.in_feature_size = list(self.bert.parameters())[0].shape[1]
+        self.lin_pos = nn.Linear(self.in_feature_size, pos_out_size)
+        self.lin_um = nn.Linear(self.in_feature_size, um_out_size)
+        self.lin_lem = nn.Linear(self.in_feature_size, lem_vocab_size)
 
     @staticmethod
     def _calculate_chunks(tokenized):
@@ -39,113 +40,69 @@ class UDTube(nn.Module):
             tokens = slice(start, end)
             if not chunks or chunks[-1] != tokens:
                 chunks.append(tokens)
-        return chunks
+        yield chunks
 
     def get_word_embs(self, tokenized):
         """get the embeddings for each word (not just subwords)"""
         with torch.no_grad():
             output = self.bert(**tokenized)
             hidden_states = output[2]
-        # Swap dimensions 0 and 1.
-        # Embeddings are from the last hidden state
-        token_embeddings = hidden_states[-1].permute(1, 0, 2)
-        token_embeddings = torch.squeeze(token_embeddings, dim=1)  # getting rid of batch (will I regret this?)
-        chunks = self._calculate_chunks(tokenized)
+        token_embeddings = hidden_states
         ids_and_wembs = []
-        for chunk in chunks:
+        for chunk in self._calculate_chunks(tokenized):
             ids_and_wembs.append(
                 (tokenized.input_ids[:, chunk],
-                 self._inplace_mean(token_embeddings[chunk]))
+                 torch.mean(token_embeddings[chunk], dim=1))
             )
         return ids_and_wembs
 
-    def forward(self, x, y_pos, y_um, y_lem):
-        if self.training:
-            self.bert.train()
-        else:
-            self.bert.eval()
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        # it is independent of forward
+        x, y_pos, y_um, y_lem = batch
+        x = x.view(x.size(0), -1)
+        z = self.bert(x)
 
-        x = x.to(self.device)
+        x_pos_hat = self.lin_pos(z)
+        loss = nn.CrossEntropyLoss(x_pos_hat, x)
+
+        x_um_hat = self.lin_um(z)
+        loss += nn.CrossEntropyLoss(x_um_hat, x)
+
+        x_lem_hat = self.lin_lem(z)
+        loss += nn.CrossEntropyLoss(x_lem_hat, x)
+
+        self.log("train_loss", loss)
+        return loss
+
+    def forward(self, x, y_pos, y_um, y_lem):
+        self.bert.eval()
         tokenized = self.tokenizer(x, return_tensors="pt")
         _, embs = zip(*self.get_word_embs(tokenized))
 
-        # will have to change the lemma handling here
+        res = {}
         pos_logits = self.lin_pos(embs)
-        um_logits = self.lin_um(embs)
-        lem_logits = self.lin_lem(embs)
-        layers = (pos_logits, um_logits, lem_logits)
-        names = ('pos', 'um', 'lemmas')
-        golds = (y_pos, y_um, y_lem)
+        y_pos_hat = pos_logits.argmax(-1)
+        res['pos'] = {"logits": pos_logits,
+                     "gold": y_pos,
+                     "pred": y_pos_hat}
 
-        res = dict()
-        for name, logits, y in zip(names, layers, golds):
-            y = y.to(self.device)
-            y_hat = logits.argmax(-1)
-            res[name] = {"logits": logits,
-                         "gold": y,
-                         "pred": y_hat}
+        um_logits = self.lin_um(embs)
+        y_um_hat = um_logits.argmax(-1)
+        res['pos'] = {"logits": um_logits,
+                      "gold": y_um,
+                      "pred": y_um_hat}
+
+        lem_logits = self.lin_lem(embs)
+        y_lem_hat = pos_logits.argmax(-1)
+        res['pos'] = {"logits": lem_logits,
+                      "gold": y_lem,
+                      "pred": y_lem_hat}
 
         return res
 
 
-def train(model, data, optimizer):
-    model.train()
-    prog = tqdm.auto(len(data))
-    for i, d in enumerate(data):
-        words, x, is_heads, tags, y, seqlens = data
-        _y = y  # for monitoring
-        optimizer.zero_grad()
-        res = model(x, y)
-        for task in res:
-            logits = task['logits']
-            y = task['y']
-            logits = logits.view(-1, logits.shape[-1])
-            y = y.view(-1)
-            # does this really make sense to do? Take loss of 3 tasks at once?
-            loss = nn.CrossEntropyLoss(ignore_index=0)(logits, y)
-            loss.backward()
-        optimizer.step()
-        prog.update(1)
-
-        if i % 10 == 0:  # monitoring
-            print(f"step: {i}, loss: {loss.item()}")
-
-
-# TODO implement for our purposes
-# def eval(model, data):
-#     model.eval()
-#     Words, Is_heads, Tags, Y, Y_hat = [], [], [], [], []
-#     with torch.no_grad():
-#         for i, batch in enumerate(data):
-#             words, x, is_heads, tags, y, seqlens = batch
-#
-#             _, _, y_hat = model(x, y)
-#
-#             Words.extend(words)
-#             Is_heads.extend(is_heads)
-#             Tags.extend(tags)
-#             Y.extend(y.numpy().tolist())
-#             Y_hat.extend(y_hat.cpu().numpy().tolist())
-#
-#     ## gets results and save
-#     with open("result", 'w') as fout:
-#         for words, is_heads, tags, y_hat in zip(Words, Is_heads, Tags, Y_hat):
-#             y_hat = [hat for head, hat in zip(is_heads, y_hat) if head == 1]
-#             preds = [idx2tag[hat] for hat in y_hat]
-#             assert len(preds) == len(words.split()) == len(tags.split())
-#             for w, t, p in zip(words.split()[1:-1], tags.split()[1:-1], preds[1:-1]):
-#                 fout.write("{} {} {}\n".format(w, t, p))
-#             fout.write("\n")
-#
-#     ## calc metric
-#     y_true = np.array([tag2idx[line.split()[1]] for line in open('result', 'r').read().splitlines() if len(line) > 0])
-#     y_pred = np.array([tag2idx[line.split()[2]] for line in open('result', 'r').read().splitlines() if len(line) > 0])
-#
-#     acc = (y_true == y_pred).astype(np.int32).sum() / len(y_true)
-#
-#     print("acc=%.2f" % acc)
-
-
 if __name__ == '__main__':
-    # just an example so i don't forget
-    model = UDTube('distilbert_base_uncased', 1, 2, 3)
+    parser = set_up_parser()
+    model = UDTube(parser.model, 2, 2, 2)
+
