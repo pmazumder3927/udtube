a54b7fd3c08d3b128b5505f4da6d707851b8c344
DanielYakubov <Danielyak98@gmail.com>
2024-07-04 18:14:22 -0400

    tuning functionality

--- sweep_resources/tuning.py
+++ sweep_resources/tuning.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 import yaml
 import wandb
@@ -8,8 +9,34 @@ import argparse
 
 os.environ["WANDB_AGENT_DISABLE_FLAPPING"] = "true"
 
-def get_args():
-    with open('configs/fit_config.yaml', 'r') as f:
+
+def setup_parser():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--sweep_id",
+        required=True,
+        help="ID for the sweep to run the agent in.",
+    )
+    parser.add_argument(
+        "--entity", required=True, help="The entity scope for the project."
+    )
+    parser.add_argument(
+        "--project", required=True, help="The project of the sweep."
+    )
+    parser.add_argument(
+        "--count",
+        type=int,
+        help="The max number of runs for this agent",
+    )
+    parser.add_argument(
+        "--fit_config",
+        help="directory of the train config. Default is ../configs/fit_config.yaml",
+        default="../configs/fit_config.yaml"
+    )
+    return parser
+
+def setup_run_params(train_config='../configs/fit_config.yaml'):
+    with open(train_config, 'r') as f:
         wandb.init()
         orig_config = yaml.safe_load(f)
         # Model arguments come from the wandb sweep config and override any
@@ -35,36 +62,19 @@ def get_args():
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument(
-        "--sweep_id",
-        required=True,
-        help="ID for the sweep to run the agent in.",
-    )
-    # parser.add_argument(
-    #     "--entity", required=True, help="The entity scope for the project."
-    # )
-    # parser.add_argument(
-    #     "--project", required=True, help="The project of the sweep."
-    # )
-    # parser.add_argument(
-    #     "--count",
-    #     type=int,
-    #     help="The max number of runs for this agent",
-    # )
+    parser = setup_parser()
     args = parser.parse_args()
-    # Forces log_wandb to True, so that the PTL trainer logs runtime metrics
-    # to wandb.
+    # Forces log_wandb to True, so that the PTL trainer logs runtime metrics to wandb.
     args.log_wandb = True
     try:
         wandb.agent(
             sweep_id=args.sweep_id,
-            project="HyperParamandTheSweepyHollow",
-            function=functools.partial(get_args),
+            project=args.project,
+            entity=args.entity,
+            function=functools.partial(setup_run_params, train_config=args.fit_config),
         )
     except Exception as e:
-        print(e)
-        print("pain")
+        print(e, file=sys.stderr)
         # Exits gracefully, so wandb logs the error.
         exit(1)
     finally:
