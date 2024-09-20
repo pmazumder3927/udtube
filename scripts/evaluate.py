0e776e85df43a004cf40ba6e58e4f6a1fea76fd1
Kyle Gorman <kylebgorman@gmail.com>
2024-09-20 10:52:57 -0400

    Removes off-by-one checking in evaluate.
    This does not seem to be necessary anymore; MWEs do not require special
    treatment here.

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -7,46 +7,22 @@ import logging
 from udtube import data
 
 
+class Error(Exception):
+    pass
+
+
 def main(args: argparse.Namespace) -> None:
     correct_upos = 0
     correct_xpos = 0
     correct_lemma = 0
     correct_feats = 0
     total = 0
-    for gold_list, hypo_list in zip(
-        data.parse(args.gold), data.parse(args.hypo)
+    for linenum, (gold_list, hypo_list) in enumerate(
+        zip(data.parse(args.gold), data.parse(args.hypo)), 1
     ):
-        gold_idx = 0
-        hypo_idx = 0
-        for _ in zip(gold_list, hypo_list):
-            try:
-                gold_tok = gold_list[gold_idx]
-                hypo_tok = hypo_list[hypo_idx]
-            except IndexError:
-                logging.warning(
-                    "Sequence mismatch: %r and %r", gold_list, hypo_list
-                )
-                total += 1
-                continue
-            # Attempts to fix alignment errors within +/- 1 non-UNK
-            # word.
-            # TODO: is this still necessary with the current MWE handling?
-            if (
-                gold_tok["form"] != hypo_tok["form"]
-                and "[UNK]" != hypo_tok["form"]
-            ):
-                if len(hypo_list) > hypo_idx + 1:
-                    if gold_tok["form"] == hypo_list[hypo_idx + 1]["form"]:
-                        hypo_idx += 1
-                        hypo_tok = hypo_list[hypo_idx]
-                if len(gold_list) > gold_idx + 1:
-                    if gold_list[gold_idx + 1] == hypo_tok["form"]:
-                        gold_idx += 1
-                        gold_tok = gold_list[gold_idx]
-            # Does not check multiword tokens, since they are unannotated.
-            if isinstance(gold_tok["id"], tuple):
-                if isinstance(hypo_tok["id"], tuple):
-                    continue
+        if len(gold_list) != len(hypo_list):
+            raise Error("Length mismatch on sentence %d", linenum)
+        for gold_tok, hypo_tok in zip(gold_list, hypo_list):
             correct_upos += gold_tok["upos"] == hypo_tok["upos"]
             correct_xpos += gold_tok["xpos"] == hypo_tok["xpos"]
             # We do this case-insensitively.
@@ -55,8 +31,6 @@ def main(args: argparse.Namespace) -> None:
             )
             correct_feats += gold_tok["feats"] == hypo_tok["feats"]
             total += 1
-            gold_idx += 1
-            hypo_idx += 1
     # Expresses this as a percentage as per the community preference.
     logging.info("UPOS accuracy:\t%2.2f", 100 * correct_upos / total)
     logging.info("XPOS accuracy:\t%2.2f", 100 * correct_xpos / total)
