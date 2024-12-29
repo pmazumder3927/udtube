d5ebbbc0e5791ba145ab266ebab0baddcbeb8971
Kyle Gorman <kylebgorman@gmail.com>
2024-12-29 14:54:23 -0500

    Indirectly adds metric-based testing
    This PR first improves the prediction-based testing by very lightly
    tuning the two models:
    
    * We now use language-appropriate pretrained models
    * We increase the learning rates substantially; the goal is to overfit
      here anyways
    * We now do 10 epochs of training; this is not very expensive compared
      to the time spent downloading dependencies and pretrained models
    * We actually reuse the best checkpoint (!)
    
    This gives us, to rounding, perfect resubstitution accuracy, so any
    model which gives the expected predictions has successfully overfit. To
    confirm this, we run `udtube test` and the results appear in the logs.
    
    This is an indirect, lightweight solution and closes #64.
    
    Other details:
    
    * I changed `assertFileExists` to `assertNonEmptyFileExists`, just to be
      sure. `os.stat` is very cheap.
    * I moved some testdata files around.
    * I added docs on how to regenerate the data if models ever change. This
      should make fixing this change-detector test much easier!

--- tests/testdata/datasets/README.md
+++
@@ -1,5 +0,0 @@
-This directory contains small "toy" samples drawn from Universal Dependencies
-data for English, Greek, and Russian for `udtube_test.py`. The `_train.conllu`
-files are used to train and validate the model, and the `_expected.conllu`
-files are the result of applying the model to the training data.
-Each file contains ten sentences.
--- tests/udtube_test.py
+++ tests/udtube_test.py
@@ -3,7 +3,7 @@
 This runs five epochs of training over a small toy data set, attempting to
 overfit, then compares the resubstitution predictions on this set to
 previously computed results. As such this is essentially a change-detector
-test. Currently, English (en), Greek (el), and Russian (ru) are supported."""
+test. Currently, English (en) and Russian (ru) are tested."""
 
 import difflib
 import os
@@ -17,35 +17,52 @@ from udtube import cli
 # Directory the unit test is located in, relative to the working directory.
 DIR = os.path.relpath(os.path.dirname(__file__), os.getcwd())
 CONFIG_PATH = os.path.join(DIR, "testdata/udtube_config.yaml")
-DATASETS_DIR = os.path.join(DIR, "testdata/datasets")
+TESTDATA_DIR = os.path.join(DIR, "testdata")
 
 
 class UDTubeTest(unittest.TestCase):
-    def assertFileExists(self, path: str):
+    def assertNonEmptyFileExists(self, path: str):
         self.assertTrue(os.path.exists(path), msg=f"file {path} not found")
+        self.assertGreater(
+            os.stat(path).st_size, 0, msg="file {path} is empty"
+        )
 
     def setUp(self):
         self.tempdir = tempfile.TemporaryDirectory(prefix="udtube_test-")
-        self.assertFileExists(CONFIG_PATH)
+        self.assertNonEmptyFileExists(CONFIG_PATH)
 
     def tearDown(self):
         self.tempdir.cleanup()
 
     @parameterized.expand(
         [
-            ("en", True),
-            ("ru", False),  # Russian doesn't have XPOS.
+            ("en", "google-bert/bert-base-cased", True),
+            ("ru", "DeepPavlov/rubert-base-cased", False),
         ]
     )
-    def test_model(self, langcode: str, use_xpos: bool):
+    def test_model(self, langcode: str, encoder: str, use_xpos: bool):
         # Fits model.
-        train_path = os.path.join(DATASETS_DIR, f"{langcode}_train.conllu")
-        self.assertFileExists(train_path)
+        train_path = os.path.join(TESTDATA_DIR, f"{langcode}_train.conllu")
+        self.assertNonEmptyFileExists(train_path)
         expected_path = os.path.join(
-            DATASETS_DIR, f"{langcode}_expected.conllu"
+            TESTDATA_DIR, f"{langcode}_expected.conllu"
         )
-        self.assertFileExists(expected_path)
-        model_dir = os.path.join(self.tempdir.name, "model")
+        self.assertNonEmptyFileExists(expected_path)
+        model_dir = os.path.join(self.tempdir.name, "models")
+        print(
+            " ".join(
+                [
+                    "fit",
+                    f"--config={CONFIG_PATH}",
+                    f"--data.model_dir={model_dir}",
+                    f"--data.train={train_path}",
+                    # We are trying to overfit on the training data.
+                    f"--data.val={train_path}",
+                    f"--model.encoder={encoder}",
+                    f"--model.use_xpos={use_xpos}",
+                ]
+            )
+        )
         cli.udtube_python_interface(
             [
                 "fit",
@@ -54,14 +71,15 @@ class UDTubeTest(unittest.TestCase):
                 f"--data.train={train_path}",
                 # We are trying to overfit on the training data.
                 f"--data.val={train_path}",
+                f"--model.encoder={encoder}",
                 f"--model.use_xpos={use_xpos}",
             ]
         )
+        # Confirms a checkpoint was created.
         checkpoint_path = (
-            f"{self.tempdir.name}/model/lightning_logs/"
-            "version_0/checkpoints/last.ckpt"
+            f"{model_dir}/lightning_logs/version_0/checkpoints/last.ckpt"
         )
-        self.assertFileExists(checkpoint_path)
+        self.assertNonEmptyFileExists(checkpoint_path)
         # Predicts on "expected" data.
         predicted_path = os.path.join(
             self.tempdir.name, f"{langcode}_predicted.conllu"
@@ -69,16 +87,31 @@ class UDTubeTest(unittest.TestCase):
         cli.udtube_python_interface(
             [
                 "predict",
+                f"--ckpt_path={checkpoint_path}",
                 f"--config={CONFIG_PATH}",
                 f"--data.model_dir={model_dir}",
                 f"--data.predict={expected_path}",
+                f"--model.encoder={encoder}",
                 f"--model.use_xpos={use_xpos}",
                 f"--prediction.path={predicted_path}",
             ]
         )
-        self.assertFileExists(predicted_path)
+        self.assertNonEmptyFileExists(predicted_path)
         diff = self._diff(predicted_path, expected_path)
-        self.assertEqual(diff, [], f"Differences found:\n{diff}")
+        self.assertEqual(diff, [], f"Prediction differences found:\n{diff}")
+        # Tests (i.e., evaluates) on "expected" data, which is logged.
+        tested_path = os.path.join(self.tempdir.name, f"{langcode}_.conllu")
+        cli.udtube_python_interface(
+            [
+                "test",
+                f"--ckpt_path={checkpoint_path}",
+                f"--config={CONFIG_PATH}",
+                f"--data.model_dir={model_dir}",
+                f"--data.test={expected_path}",
+                f"--model.encoder={encoder}",
+                f"--model.use_xpos={use_xpos}",
+            ]
+        )
 
     @staticmethod
     def _diff(predicted_path: str, expected_path: str) -> list:
