42e8f756edefeb0b2b62d7fcda8c67cac0d848c9
DanielYakubov <Danielyak98@gmail.com>
2025-03-22 19:20:56 -0400

    changing leakyReLU to Softmax

--- udtube/modules.py
+++ udtube/modules.py
@@ -211,7 +211,7 @@ class UDTubeClassifier(lightning.LightningModule):
         """
         return nn.Sequential(
             nn.Linear(hidden_size, out_size),
-            nn.LeakyReLU(),
+            nn.Softmax(),
         )
 
     # Properties.
